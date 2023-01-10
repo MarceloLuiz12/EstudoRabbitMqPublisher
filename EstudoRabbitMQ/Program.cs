3e73532131275222644d98dbeb582bae921177d8
Marcelo Luiz Pinheiro <marcelo_luiz17@outlook.com>
2023-01-10 20:57:15 -0300

    Add project files.

---
+++ EstudoRabbitMQ/Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace EstudoRabbitMQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "127.0.0.1" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare(queue: "Hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                try
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body.ToArray());
+                    Console.WriteLine($"[X] Received: {message}");
+
+                    channel.BasicAck(ea.DeliveryTag, false);
+                }
+                catch
+                {
+                    var retryQueue = true;
+                    channel.BasicNack(ea.DeliveryTag, false, retryQueue);
+                }
+            };
+            channel.BasicConsume(queue: "Hello", autoAck: false, consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Ex1Publisher/Program.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace Ex1Publisher
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "127.0.0.1" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare(queue: "Hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            int count = 0;
+
+            while (true)
+            {
+                string message = $"Olá Mundo! {count++}";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "Hello",
+                                     basicProperties: null,
+                                     body: body);
+
+                Console.WriteLine($"[X] Sent: {message}");
+
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
