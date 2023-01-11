5f8f6a0ddb2e0a5af9d2c0dd079fa131518abb02
Marcelo Luiz <marcelo_luiz17@outlook.com>
2023-01-11 10:13:45 -0300

    Exemplo fanaut

---
+++ EstudoRabbitMqPublisherEx2/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "127.0.0.1" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+{
+    channel.QueueDeclare(queue: "Fila1", durable: false, exclusive: false, autoDelete: false, arguments: null);
+    channel.QueueDeclare(queue: "Fila2", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+    channel.ExchangeDeclare(exchange: "logs", type: "fanout");
+
+    channel.QueueBind(queue: "text", exchange: "logs", "");
+    channel.QueueBind(queue: "external", exchange: "logs", "");
+
+    int count = 0;
+
+    while (true)
+    {
+        string message = $"Fanout message! {count++}";
+
+        var body = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish(exchange: "logs", routingKey: "", basicProperties: null, body: body);
+
+        Console.WriteLine($"[x] Sent: {message}");
+        Thread.Sleep(100);
+    }
+}
